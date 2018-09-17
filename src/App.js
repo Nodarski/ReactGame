0236fa48b0cf788f17200119f251cc4b1f0609d5
Nodarski <nodarlaw@gmail.com>
2018-09-17 12:15:13 -0500

    fixed where it would do strange stuff when images were clicked quickly

--- src/App.js
+++ src/App.js
@@ -17,11 +17,10 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      value:'',
-      score:0,
-      highScore:0,
-      imgArr:[car1, car2, car3, car4, car5, car6, car7, car8, car9, car10, car11, car12],
-      clickedArr:[],
+      score: 0,
+      highScore: 0,
+      imgArr: [car1, car2, car3, car4, car5, car6, car7, car8, car9, car10, car11, car12],
+      clickedArr: [],
       message: 'Click on any Picture to begin.. Dont forget it!'
     }
 
@@ -34,74 +33,83 @@ class App extends Component {
 
   shuffle(array) {
     var currentIndex = array.length, temporaryValue, randomIndex;
-  
+
     // While there remain elements to shuffle...
     while (0 !== currentIndex) {
-  
+
       // Pick a remaining element...
       randomIndex = Math.floor(Math.random() * currentIndex);
       currentIndex -= 1;
-  
+
       // And swap it with the current element.
       temporaryValue = array[currentIndex];
       array[currentIndex] = array[randomIndex];
       array[randomIndex] = temporaryValue;
     }
-  
+
     return array;
   }
 
 
   includes(k) {
-    for(var i=0; i < this.state.clickedArr.length; i++){
-      if( this.state.clickedArr[i] === k  ){
-        return true;
-      }
+    //use indeof, instead of for loop
+    if(this.state.clickedArr.indexOf(k)===-1){
+      return false;
+    }else{
+      return true;
     }
-    return false;
+
+
+
+    // for (var i = 0; i < this.state.clickedArr.length; i++) {
+    //   if (this.state.clickedArr[i] === k) {
+    //     return true;
+    //   }
+    // }
+    // return false;
   }
 
   handleClick(event) {
     var selected = event.target.src;
 
-    this.setState({ clickedArr: [...this.state.clickedArr, event.target.src] },()=> {
+    this.setState({ clickedArr: [...this.state.clickedArr, event.target.src] }, () => {
       console.log(this.state.clickedArr)
     })
 
-    if(this.includes(selected) === true){
-      setTimeout(()=> {
-        this.setState({score:0, clickedArr:[], message:'Click on any Picture to begin.. Dont forget it!'});
-        this.shuffle(this.state.imgArr);
-        this.forceUpdate();
-      }, 4000);
-      this.setState({score:0, clickedArr:[], message:'WRONG!! YOULOOOSE'})
-    
+    if (this.includes(selected) === true) {
+        this.setState({ score: 0, clickedArr: [], message: 'Wrong, You lose! Click any picture to try again. :)' });
+      this.setState({imgArr:this.shuffle(this.state.imgArr)}) 
+        // this.forceUpdate();
       
-     
-    
-    }else{
-      
-      this.setState({score: this.state.score +1, message: 'Such a strong memory, oh my!'},()=>{
-        if(this.state.highScore<this.state.score){
-          this.setState({highScore:this.state.score});
-        }
-        if(this.state.score === 12){
-          this.setState({message: 'CONGRATS, YOU HAVE WON!! play again.'})
 
-          setTimeout(()=> {
-            this.setState({score:0, clickedArr:[], message:'Click on any Picture to begin.. Dont forget it!'});
-           
+
+
+
+    } else {
+
+      this.setState({ score: this.state.score + 1, message: 'Such a strong memory, oh my!' }, () => {
+        if (this.state.highScore < this.state.score) {
+          this.setState({ highScore: this.state.score });
+        }
+        if (this.state.score === 12) {
+          this.setState({ message: 'CONGRATS, YOU HAVE WON!! play again.' })
+
+          setTimeout(() => {
+            this.setState({ score: 0, clickedArr: [], message: 'Click on any Picture to begin.. Dont forget it!' });
+
           }, 4000);
         }
-        this.shuffle(this.state.imgArr)
-        this.forceUpdate()
+        this.setState({imgArr:this.shuffle(this.state.imgArr)}) 
+
+        // this.shuffle(this.state.imgArr)
+        // this.forceUpdate()
       });
 
     };
-    
+
   }
 
-  
+
   render() {
     return (
       <div className="App">
@@ -112,21 +120,22 @@ class App extends Component {
         <p className="App-intro">
           {this.state.message}
         </p>
-        <img src={this.state.imgArr[1]}  alt='car' onClick={this.handleClick}/>
-        <img src={this.state.imgArr[2]}  alt='car' onClick={this.handleClick}/>
-        <img src={this.state.imgArr[11]}  alt='car' onClick={this.handleClick}/>
-        <img src={this.state.imgArr[3]}  alt='car' onClick={this.handleClick}/>
-        <br/>
-        <img src={this.state.imgArr[4]}  alt='car' onClick={this.handleClick}/>
-        <img src={this.state.imgArr[10]}  alt='car' onClick={this.handleClick}/>
-        <img src={this.state.imgArr[5]}  alt='car' onClick={this.handleClick}/>
-        <img src={this.state.imgArr[6]}  alt='car' onClick={this.handleClick}/>
-        <br/>
+        
+        <img src={this.state.imgArr[1]} alt='car' onClick={this.handleClick} />
+        <img src={this.state.imgArr[2]} alt='car' onClick={this.handleClick} />
+        <img src={this.state.imgArr[11]} alt='car' onClick={this.handleClick} />
+        <img src={this.state.imgArr[3]} alt='car' onClick={this.handleClick} />
+        <br />
+        <img src={this.state.imgArr[4]} alt='car' onClick={this.handleClick} />
+        <img src={this.state.imgArr[10]} alt='car' onClick={this.handleClick} />
+        <img src={this.state.imgArr[5]} alt='car' onClick={this.handleClick} />
+        <img src={this.state.imgArr[6]} alt='car' onClick={this.handleClick} />
+        <br />
 
-        <img src={this.state.imgArr[7]}  alt='car' onClick={this.handleClick}/>
-        <img src={this.state.imgArr[9]}  alt='car' onClick={this.handleClick} />
-        <img src={this.state.imgArr[8]}  alt='car' onClick={this.handleClick}/>
-        <img src={this.state.imgArr[0]}  alt='car' onClick={this.handleClick}/>
+        <img src={this.state.imgArr[7]} alt='car' onClick={this.handleClick} />
+        <img src={this.state.imgArr[9]} alt='car' onClick={this.handleClick} />
+        <img src={this.state.imgArr[8]} alt='car' onClick={this.handleClick} />
+        <img src={this.state.imgArr[0]} alt='car' onClick={this.handleClick} />
 
       </div>
     );
